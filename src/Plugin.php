16070609fee64e45502fb5f2122961572305c949
Marijn Ophorst <marijn@monomelodies.nl>
2017-04-17 08:21:05 +0200

    let this handle the primary keys (where they belong). also, store requestedIndexes on the object so extending plugins (like mysql) can alter it

--- src/Plugin.php
+++ src/Plugin.php
@@ -15,12 +15,13 @@ use PDO;
 abstract class Plugin extends Core\Plugin
 {
     const REGEX = "@^CREATE\s+(UNIQUE\s+)?INDEX\s+([^\s]+?)?\s*ON\s+([^\s\(]+)(\s+USING \w+)?\s*\((.*)\).*?;$@m";
+    const DEFAULT_INDEX_TYPE = '';
 
     public $description = 'Checking index (re)creation...';
+    protected $requestedIndexes = [];
 
     public function __invoke(string $sql) : string
     {
-        $requestedIndexes = [];
         if (preg_match_all(static::REGEX, $sql, $matches, PREG_SET_ORDER)) {
             foreach ($matches as $index) {
                 $name = strlen($index[2])
@@ -29,7 +30,7 @@ abstract class Plugin extends Core\Plugin
                 $index[5] = preg_replace("@,\s+@", ',', $index[5]);
                 $index[1] = trim($index[1]);
                 $index[4] = trim($index[4]);
-                $requestedIndexes[$name] = $index;
+                $this->requestedIndexes[$name] = $index;
                 $sql = str_replace($index[0], '', $sql);
             }
         }
@@ -38,24 +39,29 @@ abstract class Plugin extends Core\Plugin
                 $index[5] = $index[4];
                 $name = "{$index[1]}_PRIMARY";
                 $index[4] = $name;
-                $requestedIndexes[$name] = $index;
+                $this->requestedIndexes[$name] = $index;
                 $sql = str_replace($index[0], '', $sql);
             }
         }
-        if (preg_match_all("@^CREATE TABLE\s+([^\s]+?)\s+\((.*?)^\)@ms", $sql, $pktables, PREG_SET_ORDER)) {
+        if (preg_match_all("@^CREATE TABLE\s+([^\s]+?)\s+\($(.*?)^\)@ms", $sql, $pktables, PREG_SET_ORDER)) {
             foreach ($pktables as $pktable) {
                 if (preg_match("@^\s+([^\s]+).*?PRIMARY KEY@m", $pktable[0], $pk)
                     || preg_match("@^\s+PRIMARY KEY\((.*?)\)@m", $pktable[0], $pk)
                 ) {
                     $name = "{$pktable[1]}_PRIMARY";
-                    $requestedIndexes[$name] = [
+                    $this->requestedIndexes[$name] = [
                         "ALTER TABLE {$pktable[1]} ADD PRIMARY KEY({$pk[1]})",
                         'UNIQUE',
                         '',
                         '',
-                        $name,
-                        $pk[1]
+                        static::DEFAULT_INDEX_TYPE,
+                        preg_replace('@,\s+@', ',', $pk[1])
                     ];
+                    if (substr(trim($pk[0]), 0, 11) == 'PRIMARY KEY') {
+                        $pktable[0] = str_replace($pk[0], '', $pktable[0]);
+                        $corrected = preg_replace('@,$^\)@m', "\n)", $pktable[0]);
+                        $sql = str_replace($pktable[0], $corrected, $sql);
+                    }
                 }
             }
         }
@@ -65,11 +71,11 @@ abstract class Plugin extends Core\Plugin
             if ($old['index_name'] == 'PRIMARY') {
                 $old['index_name'] = "{$old['table_name']}_PRIMARY";
             }
-            if (!isset($requestedIndexes[$old['index_name']])
-                || strtolower($old['column_name']) != strtolower($requestedIndexes[$old['index_name']][5])
-                || $old['non_unique'] != ($requestedIndexes[$old['index_name']][1] == 'UNIQUE' ? 0 : 1)
+            if (!isset($this->requestedIndexes[$old['index_name']])
+                || strtolower($old['column_name']) != strtolower($this->requestedIndexes[$old['index_name']][5])
+                || $old['non_unique'] != ($this->requestedIndexes[$old['index_name']][1] == 'UNIQUE' ? 0 : 1)
                 || (!preg_match("@_PRIMARY$@", $old['index_name'])
-                    && strtolower($old['type']) != strtolower($requestedIndexes[$old['index_name']][4])
+                    && strtolower($old['type']) != strtolower($this->requestedIndexes[$old['index_name']][4])
                 )
             ) {
                 // Index has changed, so it needs to be rebuilt.
@@ -80,10 +86,10 @@ abstract class Plugin extends Core\Plugin
                 }
             } else {
                 // Index is already there, so ignore it.
-                unset($requestedIndexes[$old['index_name']]);
+                unset($this->requestedIndexes[$old['index_name']]);
             }
         }
-        foreach ($requestedIndexes as $index) {
+        foreach ($this->requestedIndexes as $index) {
             $this->defer($index[0]);
         }
         return $sql;
